08eae5c0c29febbe3626ba4f12986485222433c7
Italo Gustavo <itamelo555@gmail.com>
2026-01-30 12:57:25 -0300

    feat: integration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,21 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "next-themes";
+import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { EmailAnalyzer } from "@/components/EmailAnalyzer";
+
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <p>Hello</p>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <TooltipProvider>
+          <EmailAnalyzer />
+          <Toaster />
+        </TooltipProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
